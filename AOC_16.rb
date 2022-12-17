ae6f1598b80a766c14a0f4d430e3fa8fc5e0947b
Lynn Maxwell <lynnmaxwell@gmail.com>
2022-12-17 11:34:41 -0500

    this doesn't work at all

--- AOC_16.rb
+++ AOC_16.rb
@@ -1,6 +1,4 @@
 def read_file
-  nodes = []
-  length = 0
   graph = {}
   flow = {}
 
@@ -20,14 +18,57 @@ def read_file
     graph[node] = neighbors
     flow[node] = flow_rate
   end
+
   [graph, flow]
 end
 
-def path
-  v1 = 'AA'
+def graph_with_paths(_graph, flow)
+  graph = _graph
+  graph_paths = {}
+  graph_paths['AA'] = 'AA'
+  distance = {}
+  distance['AA'] = 0
+  open_list = ['AA']
+  pass_through = []
+  while open_list.any?
+    current = open_list[0]
+    puts graph[current]
+    graph[current].each do |neighbor, _i|
+      unless graph_paths.include? neighbor
+        if flow[neighbor] == 0 || flow[neighbor].nil?
+          pass_through.push(neighbor)
+        else
+          graph_paths[neighbor] = graph_paths[current] + neighbor
+          distance[neighbor] = 1 + distance[current]
+          open_list.push(neighbor)
+        end
+
+      end
+    end
+  end
+
+  def path
+    v1 = 'AA'
+    distance_from_start = {}
+    open_list = [AA]
+    distance_from_start[AA] = flow_rate[AA]
+
+    while open_list.length > 0
+      current = open_list[0]
+      open_list.shift
+      graph[current].each_with_index do |neighbor, _i|
+        unless distance_from_start.include?(neighbor)
+          distance_from_start[neighbor] = distance_from_start[current] + flow_rate[neighbor]
+          open_list.push(neighbor)
+        end
+      end
+    end
+    all_starts
+  end
 end
 
 graph, flow = read_file
+graph_with_paths(graph, flow)
 
 puts graph
 puts flow
--- trail.rb
+++ trail.rb
@@ -1,105 +1,91 @@
 def read_file
-  nodes = Array.new 
-  length=0
-
-  lines = File.readlines("terrain.txt")
+  nodes = []
+  length = 0
+  lines = File.readlines('terrain.txt')
   lines.each do |line|
-    line=line.strip
-    length=line.length
+    line = line.strip
+    length = line.length
     line.each_char do |char|
       nodes.push(char)
     end
   end
-  return nodes, length
+  [nodes, length]
 end
 
 def display_terrain(nodes, length)
-  n=0
+  n = 0
   while nodes[n]
-    print nodes[n..(n+(length-1))].join
+    print nodes[n..(n + (length - 1))].join
     puts
-    n+=length
+    n += length
   end
 end
 
-def make_graph (nodes, length)
-  graph = Hash.new
-  neighbors = Array.new
-  possible_starts = Array.new
+def make_graph(nodes, length)
+  graph = {}
+  neighbors = []
+  possible_starts = []
 
-  starting_loc = nodes.index("S")
-  nodes[starting_loc]="a"
-  destination = nodes.index("E")
-  nodes[destination]="z"
+  starting_loc = nodes.index('S')
+  nodes[starting_loc] = 'a'
+  destination = nodes.index('E')
+  nodes[destination] = 'z'
 
   nodes.each_with_index do |node, i|
-    #check if neighbors to the right
-    if (i%length)!=(length-1)
-        if nodes[i+1].ord < node.ord
-          neighbors.push(i+1)
-        elsif nodes[i+1].ord - node.ord <=1
-          neighbors.push(i+1)
-        end
-    end
-    #check if neighbors to the left
-    if (i%length)>0
-      if nodes[i-1].ord < node.ord
-        neighbors.push(i-1)
-      elsif nodes[i-1].ord - node.ord <=1
-        neighbors.push(i-1)
+    # check if neighbors to the right
+    if (i % length) != (length - 1)
+      if nodes[i + 1].ord < node.ord
+        neighbors.push(i + 1)
+      elsif nodes[i + 1].ord - node.ord <= 1
+        neighbors.push(i + 1)
       end
     end
-    #check if neighbors above
-    if i >=length
-      if (nodes[i-length].ord < node.ord) || (nodes[i-length].ord - node.ord <=1)
-        neighbors.push(i-length)
+    # check if neighbors to the left
+    if (i % length) > 0
+      if nodes[i - 1].ord < node.ord
+        neighbors.push(i - 1)
+      elsif nodes[i - 1].ord - node.ord <= 1
+        neighbors.push(i - 1)
       end
     end
-    #check if neighbors below
-    if (i<nodes.length-length)
-      if (nodes[i+length].ord < node.ord) || (nodes[i+length].ord - node.ord <=1)
-        neighbors.push(i+length)
-      end
+    # check if neighbors above
+    if i >= length && ((nodes[i - length].ord < node.ord) || (nodes[i - length].ord - node.ord <= 1))
+      neighbors.push(i - length)
     end
-    if node == "a"
-    possible_starts.push(i)
-    end  
-    graph[i]=neighbors
-    neighbors = Array.new
+    # check if neighbors below
+    if i < nodes.length - length && ((nodes[i + length].ord < node.ord) || (nodes[i + length].ord - node.ord <= 1))
+      neighbors.push(i + length)
+    end
+    possible_starts.push(i) if node == 'a'
+    graph[i] = neighbors
+    neighbors = []
   end
   print possible_starts
   path = path(graph, possible_starts, destination)
-  print path.min_by {|k, v| v}
+  print path.min_by { |_k, v| v }
 end
 
 def path(graph, possible_starts, destination)
-  all_starts = Hash.new
+  all_starts = {}
   possible_starts.each do |s|
-  distance_from_start = Hash.new
-  open_list = [s]
-  distance_from_start[s] = [s]
-  while open_list.length > 0
-    current = open_list[0]
-    open_list.shift
-    graph[current].each_with_index do |neighbor, i|
-      unless distance_from_start.include?(neighbor)
+    distance_from_start = {}
+    open_list = [s]
+    distance_from_start[s] = [s]
+    while open_list.length > 0
+      current = open_list[0]
+      open_list.shift
+      graph[current].each_with_index do |neighbor, _i|
+        next if distance_from_start.include?(neighbor)
+
         distance_from_start[neighbor] = distance_from_start[current] + [neighbor]
-        if neighbor == destination
-          all_starts[s] = distance_from_start[destination].length-1
-        end
+        all_starts[s] = distance_from_start[destination].length - 1 if neighbor == destination
         open_list.push(neighbor)
       end
     end
-    end
   end
-  return all_starts
+  all_starts
 end
 
-
-
-
-
 nodes, length = read_file
-#display_terrain(nodes, length)
+# display_terrain(nodes, length)
 make_graph(nodes, length)
-
