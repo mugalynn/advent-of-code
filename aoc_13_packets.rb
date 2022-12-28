93f603f8577c0bb9a40b032562a551839c5de196
Lynn Maxwell <lynnmaxwell@gmail.com>
2022-12-28 16:47:24 -0500

    parsed

--- aoc_13_packets.rb
+++ aoc_13_packets.rb
@@ -1,44 +1,39 @@
 def read_file
+  pairs=[]
+  input = []
   lines = File.readlines('aoc_13_shorter_input.txt')
-  pairs = []
-  individual_pair = []
-  
-  lines.each_with_index do |line, index|
-    if line.kind_of?(Array)
-      next
-    end
-    line = line.strip
-    num = line.count('[')
-    n=index
-    while individual_pair.length < 2 && num >0
-      if num == 1
-      lines[n] = lines[n].tr('[]', '')
-      lines[n] = lines[n].split ","
-      lines[n] = lines[n].map(&:to_i)
-      individual_pair.append(lines[n])
-      n+=1
-      end
-      if num > 1
-        lines[n] = lines[n].sub!("[", '')
-        remove = lines[n].rindex(']')
-        lines[n].slice!(remove)
-        number_of_lists = num-1
-        while number_of_lists>0
-          list = Array.new
-          lines[n].sub!("[", '')
-          lines[n].slice!(lines[n].rindex(']'))
-          lists = lines[n].split "],["
-          number_of_lists -=2
-        end
-        n+=1
-      end
-    end
-    if individual_pair.length == 2
-      pairs.append(individual_pair)
-      individual_pair = Array.new
-    end
-    
+  lines.each do |line|
+   line=line.strip
+   
+   unless line.empty?
+  line = line.sub!("[", '')
+   remove = line.rindex(']')
+   line.slice!(remove)
+   line = my_scan(line)
+   if pairs.length < 2
+      pairs.append(line)
+   end
+   if pairs.length == 2
+      input.append(pairs)
+      pairs = Array.new
+   end  
   end
 end
+print input[1]
+end
+
+def my_scan s
+  res = []
+  s.scan(/((\d+)|(\[(.+)\]))/) do |match|
+    if match[1]
+      res << match[1].to_i
+    elsif match[3]
+      res << my_scan(match[3])
+    end
+    end
+    res
+  end
+  
+
 
 read_file
