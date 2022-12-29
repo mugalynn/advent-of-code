37bfed46be382c77d1c2259bfdfdd1936e31238c
Lynn Maxwell <lynnmaxwell@gmail.com>
2022-12-28 22:18:58 -0500

    part 1 works

--- aoc_13_packets.rb
+++ aoc_13_packets.rb
@@ -1,15 +1,14 @@
+require 'json'
+
 def read_file
   pairs=[]
   input = []
-  lines = File.readlines('aoc_13_shorter_input.txt')
+  lines = File.readlines('aoc_13_input.txt')
   lines.each do |line|
    line=line.strip
    
    unless line.empty?
-  line = line.sub!("[", '')
-   remove = line.rindex(']')
-   line.slice!(remove)
-   line = my_scan(line)
+   line = JSON.parse(line)
    if pairs.length < 2
       pairs.append(line)
    end
@@ -19,21 +18,64 @@ def read_file
    end  
   end
 end
-print input[1]
+return input
 end
 
-def my_scan s
-  res = []
-  s.scan(/((\d+)|(\[(.+)\]))/) do |match|
-    if match[1]
-      res << match[1].to_i
-    elsif match[3]
-      res << my_scan(match[3])
-    end
-    end
-    res
+def compare(left, right, index)
+  if right.nil?
+    return 0
   end
+  if left.nil? 
+    return index
+  end
+  if right.is_a?(Integer)
+    right = [right]
+  end
+  if left.is_a?(Integer)
+    left = [left]
+  end
+  # might need code for evaluating integers? Definitely need code that converts an integer to a list.
   
+  left.each_with_index do |v, i|
+    if (v.is_a?(Integer)) && (right[i].is_a?(Integer))
+      if v>right[i]
+        return 0
+      elsif v<right[i]
+        return index
+      elsif v == right[i]
+        next
+      end 
+    elsif (v.is_a?(Integer)) && (right[i].nil?)
+      return 0
+    elsif (v.nil? && right[i].is_a?(Integer))
+      return index
+    elsif (v.kind_of?(Array)) && (right[i].kind_of?(Array))
+       output = compare(v, right[i], index)
+       return output if output != "equal"
+    elsif right[i].kind_of?(Array) && v.is_a?(Integer)
+      output = compare([v], right[i], index)
+       return output if output != "equal"
+    elsif v.kind_of?(Array) && right[i].is_a?(Integer) 
+      output = compare(v, [right[i]], index) 
+      return output if output != "equal"
+    end 
+  end
+  if left.length < right.length
+    return index
+  elsif right.length < left.length
+    return 0
+  else
+  return ("equal")
+  end
+end
 
 
-read_file
+input = read_file
+sum = 0
+input.each_with_index do |pair, i|
+  index = i+1
+  add = compare(pair[0], pair[1], index)
+  sum += add
+  
+end
+puts sum
